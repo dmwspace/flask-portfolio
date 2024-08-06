d20fb53f5c44715320ac074caddd199e00874b20
Dean <dmwspace@gmail.com>
2024-08-06 03:20:21 -0700

    Routes

--- app.py
+++ app.py
@@ -3,5 +3,22 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
+@app.route("/home")
 def base_page():
     return render_template('base.html')
+
+@app.route("/contact")
+def contact_page():
+    return render_template('contact.html')
+
+@app.route("/skills")
+def skills_page():
+    return render_template('skills.html')
+
+@app.route("/about")
+def about_page():
+    return render_template('about.html')
+
+@app.route("/projects")
+def projects_page():
+    return render_template('projects.html')
